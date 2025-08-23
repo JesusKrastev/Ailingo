578326c87f0d9ede0d041a426bda81e8ef382ffd
JesusKrastev <jesuskrastev@gmail.com>
2025-08-23 08:41:49 +0200

    feat: Introduce NonlazyGrid composable
    This commit introduces a new composable function `NonlazyGrid` located in `app/src/main/java/com/jesuskrastev/ailingo/ui/composables/NonlazyGrid.kt`.
    
    The `NonlazyGrid` composable arranges its items in a grid layout. It is "non-lazy," meaning it renders all its items at once, unlike `LazyVerticalGrid`.
    
    Key features:
    - Takes `columns`, `itemCount`, `verticalArrangement`, `horizontalArrangement`, and a `content` lambda as parameters.
    - Arranges items in a `Column` of `Row`s.
    - Each `Row` contains a number of items equal to the specified `columns`.
    - Items within each row are distributed evenly using `Modifier.weight(1f)`.
    - Handles cases where the `itemCount` is not perfectly divisible by the number of `columns`.

---
+++ app/src/main/java/com/jesuskrastev/ailingo/ui/composables/NonlazyGrid.kt
@@ -0,0 +1,50 @@
+package com.jesuskrastev.ailingo.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NonlazyGrid(
+    modifier: Modifier = Modifier,
+    columns: Int,
+    itemCount: Int,
+    verticalArrangement: Arrangement.Vertical,
+    horizontalArrangement: Arrangement.Horizontal,
+    content: @Composable() (Int) -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = verticalArrangement,
+    ) {
+        var rows = (itemCount / columns)
+        if (itemCount.mod(columns) > 0) {
+            rows += 1
+        }
+
+        for (rowId in 0 until rows) {
+            val firstIndex = rowId * columns
+
+            Row(
+                horizontalArrangement = horizontalArrangement,
+            ) {
+                for (columnId in 0 until columns) {
+                    val index = firstIndex + columnId
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        if (index < itemCount) {
+                            content(index)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
